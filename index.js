500daf955dfad1c2984537f0a8291ef37e81eca3
vlad <vlad@MacBook-Pro-vlad.local>
2020-11-24 23:15:09 +0200

    add new index files

---
+++ index.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const menuBtnBurger = document.querySelector('.menu-btn__burger');
